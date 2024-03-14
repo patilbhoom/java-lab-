d5ce4f98b0ed2d3f821bc88ca084371252439284
patilbhoom <161194371+patilbhoom@users.noreply.github.com>
2024-03-14 20:09:10 +0530

    w2

---
+++ PosorNeg.java
@@ -0,0 +1,20 @@
+package w2;
+public class PosorNeg 
+{
+	public static void main(String args[])
+	{
+		int n=82;
+		if(n>0)
+		{
+			System.out.println("The given number"+n+"is positive");
+		}
+		else if(n<0)
+		{
+		System.out.println("The given number"+n+"is negative");
+		}
+		else
+		{
+			System.out.println("The given number"+n+"is zero");
+		}
+	}
+}
