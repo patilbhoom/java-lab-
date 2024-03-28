15e0f8d495abdf483f97ce4a7d7b61e3c736d128
patilbhoom <161194371+patilbhoom@users.noreply.github.com>
2024-03-28 07:27:43 +0530

    w8

---
+++ demoMultilevel.java
@@ -0,0 +1,47 @@
+package w8;
+class Vehicle 
+{
+	protected String vehicletype;
+	private int maxSpeed;
+	Vehicle(int mSpeed)
+	{
+		maxSpeed=mSpeed;
+	}
+	public void speed()
+	{
+		System.out.println("It can cruise up to maxSpeed of"+maxSpeed+" km/hour");
+	}
+}
+class Toyota extends Vehicle
+{
+	protected String brand;
+	Toyota(int mSpeed)
+	{
+		super(mSpeed);
+	}
+}
+class Fortuner extends Toyota 
+{
+	private String model;
+	Fortuner(String vtype,int ms,String br,String mod)
+	{
+		super(ms);
+		vehicletype=vtype;
+		model=mod;
+		brand=br;
+	}
+		public void display()
+		{
+			System.out.println("the name of the vehicle is"+model);
+			System.out.println("the brand is"+brand);
+			System.out.println("the type of vehicle is"+vehicletype);
+			this.speed();
+		}
+}
+class demomultilevel 
+{
+	public static void main(String[] args) {
+		Fortuner sigma=new Fortuner("Four wheeler",280,"Toyota","Fortuner");
+		sigma.display();
+	}
+}
